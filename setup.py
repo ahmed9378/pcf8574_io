7eee326095bce966e5a135d781144f18c690ec9c
ahmed9378 <ahmed-eng@outlook.com>
2020-10-27 11:10:24 +0800

    simple changes

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup, find_packages
 
 classifiers = [
-    'Development Status :: 1 - Planning',
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Education',
+    'Operating System :: Other OS',
+    'Topic :: System :: Hardware :: Hardware Drivers',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3'
 ]
 
 setup(
     name='pcf8574_io',
-    version='',
+    version='0.0.2',
     description='setup pin mode and read, write to PCF8574 pins',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='',
     author='Ahmed Omar',
     author_email='ahmed.bm78@gmail.com',
@@ -19,5 +21,5 @@ setup(
     classifiers=classifiers,
     keywords='PCF8574',
     packages=find_packages(),
-    install_requires=['']
+    install_requires=['smbus2']
 )
