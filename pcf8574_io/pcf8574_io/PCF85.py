82787b4c80991d4dc65481cb2a259174567a9cae
alexandrefernandesjs <alexandrefernandesjs@gmail.com>
2021-06-14 16:27:16 -0300

    update

--- pcf8574_io/pcf8574_io/PCF85.py
+++ pcf8574_io/pcf8574_io/PCF85.py
@@ -29,10 +29,10 @@ def set_mode(pnNum, mode, rValue, flg):
 def digitalRead(pinName, smbs, addr):
     with SMBus(smbs) as bus:
         b = bus.read_byte(addr)
-    # if isKthBitSet(b, pinNameToNum(pinName) + 1):
+    if isKthBitSet(b, pinNameToNum(pinName) + 1):
         return b
-    # else:
-        # return False
+    else:
+        return 0
 
 
 def pinNameToNum(pinName):
